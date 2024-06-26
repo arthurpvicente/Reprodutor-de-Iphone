06a44bd9940e0a22066c67bcadbc82ddc2b5271d
Arthur Vicente <arthur.p.v@hotmail.com>
2024-06-26 17:03:13 -0300

    Projeto Final

---
+++ src/Iphone.java
@@ -0,0 +1,66 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música...");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+        
+        // Testando ReprodutorMusical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Música Exemplo");
+
+        // Testando AparelhoTelefonico
+        meuIphone.ligar("123-456-7890");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando NavegadorInternet
+        meuIphone.exibirPagina("https://www.exemplo.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
